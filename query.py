7dc723c7a73be0aeac2d65cc73d89f02c051d11b
jsiewierski11 <jarid.siewierski@gmail.com>
2020-04-30 18:51:14 -0600

    Fixing your mom recursion error.

--- query.py
+++ query.py
@@ -7,7 +7,7 @@ db = mongo_client.programr
 col = db.conversations
 
 query = {"image" : { "filename" : "This_is_the_default", "duration" : None } }
-result = col.find( query )
+result = col.find(  )
 
 for x in result:
     print(x)
